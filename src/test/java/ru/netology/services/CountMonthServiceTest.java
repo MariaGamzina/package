d5bdefcc313c432fcac7fadbbbb8bf38cafc01aa
Maria Gamzina <gamzinamaria@gmail.com>
2024-04-10 22:38:07 +0300

    add parameterized test

--- src/test/java/ru/netology/services/CountMonthServiceTest.java
+++ src/test/java/ru/netology/services/CountMonthServiceTest.java
@@ -2,27 +2,32 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountMonthServiceTest {
-    @Test
-    public void CountMonthMin() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/count.csv")
+    public void CountMonthMin(int expected, int income, int expenses, int threshold) {
         CountMonthService service = new CountMonthService();
 
-        int expected = 3;
-        int actual = service.calCount(10000, 3000, 20000);
-
-        Assertions.assertEquals(expected, actual);
-
-    }
-
-    @Test
-    public void CountMonthMax() {
-        CountMonthService service = new CountMonthService();
-
-        int expected = 2;
-        int actual = service.calCount(100_000, 60_000, 150_000);
+        // int expected = 3;
+        int actual = service.calCount(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
 
     }
 }
+
+//    @Test
+//    public void CountMonthMax() {
+//        CountMonthService service = new CountMonthService();
+//
+//        int expected = 2;
+//        int actual = service.calCount(100_000, 60_000, 150_000);
+//
+//        Assertions.assertEquals(expected, actual);
+//
+//    }
+//}
